03282f44de8fbd899a0fc7aa59b1c3430d534c67
Alexis Francois <francoisalexis.pro@gmail.com>
2021-02-26 13:51:21 +0100

    cocktail model runs

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -3,16 +3,36 @@ class CocktailsController < ApplicationController
     @cocktails = Cocktail.all
   end
 
+  def show
+    set_cocktail
+  end
+
   def new
     @cocktail = Cocktail.new
   end
 
   def create
+    @cocktail = Cocktail.new(cocktail_params)
+    @cocktail.save
   end
 
-  def show
+  # def edit
+  #   set_cocktail
+  # end
+
+  # def update
+  #   set_cocktail
+  #   @cocktail.update(cocktail_params)
+  #   redirect_to cocktail_path(@cocktail)
+  # end
+
+private
+
+  def set_cocktail
+    @cocktail = Cocktail.find(params[:id])
   end
 
-  def delete
+  def cocktail_params
+    params.require(:cocktail).permit(:name)
   end
 end
